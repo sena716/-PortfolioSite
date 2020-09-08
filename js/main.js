59d5bde34e7753068ac5ff4ac1b736abef840c7e
冨山　聖那 <19s020kr@gmail.com>
2020-09-09 00:24:46 +0900

    作成中

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
-// Skills Bars
-$('.skill-percent').each(function(){
-$(this).animate({
-    width:$(this).attr('data-percent')},"fast");
+// モーダルウィンドウ
+function work1(){
+    $('#modalWindow').fadeIn();
+    $('#modalClose, #modalBackground').click(function(){
+        $('#modalWindow').fadeOut();
+    });
+}
+
+// ハンバーガーメニュー
+$(function() {
+　  $('.Toggle').click(function() {
+        $(this).toggleClass('active');
+        $('.menu').toggleClass('open');
+        $('#header').toggleClass('active');
+    }); 
+    $('.nav').click(function(){
+        $('.Toggle').toggleClass('active');
+        $('.menu').toggleClass('open');
+        $('#header').toggleClass('active');
+    });
 });
