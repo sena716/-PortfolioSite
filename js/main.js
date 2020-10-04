77703443eae84124730ec0d97a1734f4d8797ef3
19s020kr <19s020kr@gmail.com>
2020-10-04 09:36:29 +0900

    レスポンシブ対応

--- js/main.js
+++ js/main.js
@@ -1,8 +1,29 @@
 // モーダルウィンドウ
 function work1(){
-    $('#modalWindow').fadeIn();
-    $('#modalClose, #modalBackground').click(function(){
-        $('#modalWindow').fadeOut();
+    $('#work1modalWindow').fadeIn();
+    $('#work1modalClose, #work1modalBackground').click(function(){
+        $('#work1modalWindow').fadeOut();
+    });
+}
+
+function work2(){
+    $('#work2modalWindow').fadeIn();
+    $('#work2modalClose, #work2modalBackground').click(function(){
+        $('#work2modalWindow').fadeOut();
+    });
+}
+
+function work3(){
+    $('#work3modalWindow').fadeIn();
+    $('#work3modalClose, #work3modalBackground').click(function(){
+        $('#work3modalWindow').fadeOut();
+    });
+}
+
+function work4(){
+    $('#work4modalWindow').fadeIn();
+    $('#work4modalClose, #work4modalBackground').click(function(){
+        $('#work4modalWindow').fadeOut();
     });
 }
 
@@ -12,10 +33,12 @@ $(function() {
         $(this).toggleClass('active');
         $('.menu').toggleClass('open');
         $('#header').toggleClass('active');
+        $('#header').toggleClass('aa');
     }); 
     $('.nav').click(function(){
         $('.Toggle').toggleClass('active');
         $('.menu').toggleClass('open');
         $('#header').toggleClass('active');
+        $('#header').toggleClass('aa');
     });
 });
